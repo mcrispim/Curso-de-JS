f3fc566da93152c534e8df775543aecb7ae574c6
Mauro Crispim <mcrispim@hotmail.com>
2020-09-19 12:16:29 -0300

    Aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`[${c}] Tudo bem?`)
+    c++
+}
+*/
+
+var c = 1
+do {
+    console.log(`[${c}] Tudo bem?`)
+    c++
+} while (c <= 6)
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
