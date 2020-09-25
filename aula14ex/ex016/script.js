2b46ea7f1328dafff307c2905a42a7ec3b29e322
Mauro Crispim <mcrispim@hotmail.com>
2020-09-25 19:58:01 -0300

    Falta acertar o emoji

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,12 +1,23 @@
 function contar() {
-    var start = Number(window.document.getElementById('start'));
-    var end = Number(window.document.getElementById('end'));
-    var step = Number(window.document.getElementById('step'));
+    var start = window.document.getElementById('start').value;
+    var end = window.document.getElementById('end').value;
+    var step = window.document.getElementById('step').value;
     var res = window.document.getElementById('res');
     
-    var resultado = ""
-    for (let i = start; i <= end; i += step) {
-        resultado = resultado + String(i) + ' ... ' 
-    }
-    res.innerHTML = resultado
+    if (start === "" || end === "" || step === "") {
+        res.innerHTML = "Impossível contar!";
+    } else {
+        var n_start = Number(start);
+        var n_end = Number(end);
+        var n_step = Number(step);
+        if (n_step === 0) {
+            window.alert("Passo inválido. Utilizando passo 1.");
+            n_step = 1;
+        }
+        var resultado = "";
+        for (let i = n_start; i <= n_end; i += n_step) {
+            resultado = resultado + String(i) + '&#1f449' 
+        }
+        res.innerHTML = resultado
+        }
 }
